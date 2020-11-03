94aba2eafb8bdde7b741260c7ce22c41f4a5c7a2
Jamie Voros <34633515+hamjamjam@users.noreply.github.com>
2020-11-03 12:01:09 -0700

    Update deploy-local-dev.sh

--- deploy-local-dev.sh
+++ deploy-local-dev.sh
@@ -10,8 +10,8 @@
 #
 kubectl apply -f redis/redis-deployment.yaml
 kubectl apply -f redis/redis-service.yaml
-kubectl apply -f redis/rabbitmq-deployment.yaml
-kubectl apply -f redis/rabbitmq-service.yaml
+kubectl apply -f rabbitmq/rabbitmq-deployment.yaml
+kubectl apply -f rabbitmq/rabbitmq-service.yaml
 
 kubectl port-forward --address 0.0.0.0 service/rabbitmq 5672:5672 &
 kubectl port-forward --address 0.0.0.0 service/redis-master 6379:6379 &
