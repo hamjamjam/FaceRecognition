2e618c2da86423e0c421e9e813764ce939cda8b8
Jamie Voros <34633515+hamjamjam@users.noreply.github.com>
2020-11-07 15:47:55 -0700

    Update rest-server.py

--- rest/rest-server.py
+++ rest/rest-server.py
@@ -105,7 +105,7 @@ def scanUrl(X):
                 response_pickled = jsonpickle.encode(response)
                 return Response(response=response_pickled, status=200, mimetype="application/json")
       
-    return Response(i,status=500)
+    return Response(response=i,status=500)
 
 print('pooooo')
 
