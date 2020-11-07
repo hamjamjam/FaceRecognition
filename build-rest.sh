f50b8fbef4e1a5d3c4c5f6e97534aa29de749fed
Jamie Voros <34633515+hamjamjam@users.noreply.github.com>
2020-11-07 13:48:16 -0700

    Create build-rest.sh

---
+++ build-rest.sh
@@ -0,0 +1,7 @@
+version=$1
+export PROJECT_ID=jamiess-1470077384373
+docker build -t rest:${version} .
+docker build -t gcr.io/${PROJECT_ID}/rest:${version} .
+docker push gcr.io/${PROJECT_ID}/rest:${version}
+kubectl create deployment rest --image=gcr.io/${PROJECT_ID}/rest:${version}
+kubectl expose deployment rest --type=LoadBalancer --port 5000 --target-port 5000
