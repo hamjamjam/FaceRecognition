4c999095a89bddb475f1e630c0fb65904a391811
Jamie Voros <34633515+hamjamjam@users.noreply.github.com>
2020-11-08 21:24:33 -0700

    Update worker-server.py

--- worker/worker-server.py
+++ worker/worker-server.py
@@ -34,6 +34,42 @@ redisHashToHashSet = redis.Redis(host=redisHost, db=4) # Key -> Set
 redisFaceToHashSet = redis.Redis(host=redisHost, db=5) # Key -> Set
 redisNameToHash.set('foo','bar')
 print(redisNameToHash.get('foo'))
+
+def isObama(face_encoding):
+    # Pre-calculated face encoding of Obama generated with face_recognition.face_encodings(img)
+    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
+                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
+                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
+                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
+                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
+                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
+                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
+                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
+                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
+                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
+                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
+                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
+                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
+                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
+                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
+                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
+                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
+                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
+                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
+                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
+                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
+                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
+                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
+                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
+                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
+                            0.07417042,  0.07126575,  0.00209804]
+        
+    if_obama = False
+    match_results = face_recognition.compare_faces([known_face_encoding], face_encoding)
+    if match_results[0]:
+        is_obama = True
+    return is_obama
+
         
 def callback2(ch, method, properties, inputbody):
     body = inputbody.decode("utf-8")
@@ -82,20 +118,12 @@ def callback2(ch, method, properties, inputbody):
             redisHashToFaceRec.sadd(img_hash, *face_encodings_serialized)
             print('if there were faces, added to redis')
 
-        otherHash = set()
-        print('created empty set for otherHash')
-
+        has_obama = 'no'
         for face_enc in face_encodings_serialized:
-            try:
-                otherHash.update(redisFaceToHashSet.smembers(face_enc))
-            except:
-                pass
-            redisFaceToHashSet.sadd(face_enc, img_hash)
-        print('for each face, added to face to hash, and got other hash')
-        
-        if len(otherHash) > 0:
-            redisHashToHashSet.sadd(img_hash, *otherHash)
-            print('added hash to hashset')
+            if isObama(face_enc):
+                has_obama = 'yes'
+                break
+        redisHashToObama.set(img_hash, has_obama)
 
         hashes = redisHashToHashSet.smembers(img_hash)
         print('got hashes')
@@ -159,18 +187,6 @@ def main():
 
         otherHash = {}
 
-        for face_enc in face_encodings:
-            try:
-                otherHash.add(redisFaceToHashSet.get(face_enc))
-            except:
-                pass
-            redisFaceToHashSet.sadd(face_enc, img_hash)
-
-        redisHashToHashSet.sadd(img_hash, *otherHash)
-
-        hashes = redisHashToHashSet.get(img_hash)
-        result = {responsekey: hashes    }
-        return jsonify(result)
 
 
     channel.basic_consume(queue='work', on_message_callback=wrapped_callback, auto_ack=True)
