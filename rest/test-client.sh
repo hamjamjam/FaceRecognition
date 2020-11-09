372342571437a0f3bb50573a34ed7a4d571a8247
hamjamjam <jamielsv@gmail.com>
2020-11-09 03:23:41 +0000

    updated rest/test-client.sh'

--- rest/test-client.sh
+++ rest/test-client.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+img_file="${1:-https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg}"
 IP=$(kubectl get services | grep ^rest | awk '{print $4}')
-python3 rest-client.py ${IP}:5000 url https://static.toiimg.com/photo/msid-68523832/68523832.jpg 1
+python3 rest-client.py ${IP}:5000 url "${img_file}" 1
+# https://static.toiimg.com/photo/msid-68523832/68523832.jpg 1
