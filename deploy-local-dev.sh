1ffa51a00f3d004fb25695ea0df537dfc6631b58
Jamie Voros <34633515+hamjamjam@users.noreply.github.com>
2020-11-12 11:37:41 -0700

    Update deploy-local-dev.sh

--- deploy-local-dev.sh
+++ deploy-local-dev.sh
@@ -15,15 +15,3 @@ kubectl apply -f rabbitmq/rabbitmq-service.yaml
 
 kubectl port-forward --address 0.0.0.0 service/rabbitmq 5672:5672 &
 kubectl port-forward --address 0.0.0.0 service/redis-master 6379:6379 &
-
-export PROJECT_ID=jamiess-1470077384373
-docker build -t rest:v1 .
-docker build -t gcr.io/${PROJECT_ID}/rest:v1 .
-docker push gcr.io/${PROJECT_ID}/rest:v1
-kubectl create deployment rest --image=gcr.io/${PROJECT_ID}/rest:v1
-kubectl expose deployment rest --type=LoadBalancer --port 5000 --target-port 5000
-
-docker build -t worker:v1 .
-docker build -t gcr.io/${PROJECT_ID}/worker:v1 .
-docker push gcr.io/${PROJECT_ID}/worker:v1 
-kubectl create deployment worker --image=gcr.io/${PROJECT_ID}/worker:v1
